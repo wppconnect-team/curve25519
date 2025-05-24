d06ca6e97ba8bd10ea6ef1ac124f55251bcccbc4
Edgard <edgardmessias@gmail.com>
2025-05-24 19:09:29 -0300

    chore: Updated packages

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleDirectories: ['node_modules', 'src'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testRegex: '(src/__tests__/.*|(\\.|/)(test))\\.[jt]sx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+}
